7506a230e6ef9dbfe735d153cf4e47d86155ace8
synith <synith@live.ca>
2021-07-31 11:18:15 -0600

    added background texture, adjusted player speed
    hard to tell when player is moving, added a background texture
    
    doubled players speed

--- Assets/Scripts/Starship.cs
+++ Assets/Scripts/Starship.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Starship : MonoBehaviour
 {    
-    private float speed = 15;
+    private float speed = 30;
     private int hullPoints;
     private int shieldPoints;
     private MousePosition2D mousePosition2D;
