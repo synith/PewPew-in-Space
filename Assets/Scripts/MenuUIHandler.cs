9566e2497e4a29d23a9481fe4acda796c527e9d4
synith <synith@live.ca>
2021-08-15 09:37:48 -0600

    update to Menu
    added functionality to the Start and Exit buttons of the menu
    
    Start - loads next scene
    Exit - quits application
    
    also doubled amount of pooled lasers

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
